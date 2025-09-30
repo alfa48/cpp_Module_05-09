49859208bb0a0d5d2346c0fcb17a44a7fd182d10
alfa48 <948manuel@gmail.com>
2025-09-30 08:32:43 +0100

    implement brt.cpp

--- ex00/Bureaucrat.cpp
+++ ex00/Bureaucrat.cpp
@@ -1,7 +1,51 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Bureaucrat.cpp                                     :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: manandre <manandre@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/09/30 08:28:29 by manandre          #+#    #+#             */
+/*   Updated: 2025/09/30 08:28:31 by manandre         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+
 #include "Bureaucrat.hpp"
 
-Bureaucrat::Bureaucrat(){
+Bureaucrat::Bureaucrat(): name("default"), grade(150){
+    std::cout << "Bureaucrat default construtor called, " << this->getName() << "with grade " << this->getGrade() << std::endl;
+}
 
+Bureaucrat::Bureaucrat(const Bureaucrat &brt): name(brt.name){
+    std::cout << "Bureaucrat copy constructor called, " << this->getName() << "with grade " << this->getGrade() << std::endl;
+    *this = brt;
+}
+
+Bureaucrat &Bureaucrat::operator=(const Bureaucrat &brt){
+    std::cout << "Bureaucrat assignment operator called, " << this->getName() << "with grade " << this->getGrade() << std::endl;
+    if (this == &brt){return (*this);}
+    this->grade = brt.getGrade();
+    return (*this);
+}
+
+Bureaucrat::~Bureaucrat(){
+    std::cout << "Bureaucrat destrutor called, " << this->getName() << "with grade " << this->getGrade() << std::endl;
 }
 
 
+/*
+    getters and setters 
+*/
+
+std::string Bureaucrat::getName() const{return (this->name);}
+int Bureaucrat::getGrade() const{return (this->grade);}
+
+
+/*
+    insertion operator
+*/
+std::iostream &operator<<(std::iostream &out, Bureaucrat *bureaucrat){
+    out << bureaucrat->getName() << ", bureaucrat grade" << bureaucrat->getGrade() << std::endl;
+    return (out);
+}
--- ex00/Bureaucrat.hpp
+++ ex00/Bureaucrat.hpp
@@ -1,5 +1,18 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Bureaucrat.hpp                                     :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: manandre <manandre@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/09/30 08:28:45 by manandre          #+#    #+#             */
+/*   Updated: 2025/09/30 08:28:46 by manandre         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+
 #ifndef BUREAUCRAT_H
-#define BUREAUCRAT_H\
+#define BUREAUCRAT_H
 
 /*
 Excetion é um mecanismo que permite tratar erros no programa
@@ -8,34 +21,38 @@ em execusão sem ter que interromper o programa
 
 #include <iostream>
 
-class Bureaucrat{
+class Bureaucrat
+{
 
-    private:
-        const std::string name;
-        int grade;//(ranges 1-150, 1 highest grade)
+private:
+    const std::string name;
+    int grade; //(ranges 1-150, 1 highest grade)
+
+public:
+    Bureaucrat();
+    Bureaucrat(const Bureaucrat &src);
+    Bureaucrat &operator=(const Bureaucrat &src);
+    ~Bureaucrat();
+
+    std::string getName() const;
+    int getGrade() const;
+
+    void lowerGrade();
+    void increaseGrade();
+
+    class GradeTooHighException : public std::exception
+    {
     public:
-        Bureaucrat();
-        Bureaucrat(const Bureaucrat &src);
-        Bureaucrat &operator=(const Bureaucrat &src);
-        ~Bureaucrat();
+        virtual const char *what() const throw();
+    };
 
-        std::string getName();
-        int getGrade();
-
-        void lowerGrade();
-        void increaseGrade();
-
-        class GradeTooHighException: public std::exception {
-            public:
-                virtual const char* what() const throw();
-        };
-
-        class GradeTooLowException{
-            public:
-                virtual const char* what() const throw();
-        };
+    class GradeTooLowException
+    {
+    public:
+        virtual const char *what() const throw();
+    };
 };
 
-std::ostream &operator<<(std::ostream &o, Bureaucrat *bureaucrat); 
+std::ostream &operator<<(std::ostream &o, Bureaucrat *bureaucrat);
 
 #endif
