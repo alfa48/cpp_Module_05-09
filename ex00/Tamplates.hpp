b77adb20272b57d39a9ccca6913d761ad7e965f4
alfa98 <948manuel@gmail.com>
2025-12-17 12:43:29 +0100

    init and finish 00

---
+++ ex00/Tamplates.hpp
@@ -0,0 +1,26 @@
+#ifndef TAMPLATES_HPP
+#define TAMPLATES_HPP
+
+#include <iostream>
+
+template <typename T>
+void swap(T &a, T &b){
+
+    T tmp = a;
+    a = b;
+    b = tmp;
+}
+
+template <typename T>
+T min(const T &a, const T &b){
+    if (a == b){return (b);}
+    return (a < b ? a : b);
+}
+
+template <typename T>
+T max(const T &a, const T &b){
+    if (a == b){return (b);}
+    return (a > b ? a : b);
+}
+
+#endif
